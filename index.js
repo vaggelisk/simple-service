14b27dafd96035bba5dbc0cb51e66f61dc74be79
Vagelis Karakaxis <vkarakaxis@obor-sinobridge.com>
2021-11-09 00:05:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const multer = require("multer");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const upload = multer({
+  dest: './uploads',
+});
+
+const PORT = '5000' || process.env.PORT;
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ file: req.file });
+  console.log('file uploaded');	
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
